ccaaa811e38353a4bb2c7161113e706dd51d7c2c
inceabdullah <pax.castrensis@gmail.com>
2020-09-19 20:51:41 +0300

    Added: currentTime and async mysqldump function

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const mysqldump = require('mysqldump');
 const getTimes = require('date-assistant');
 
 const DBcredentials = require('./DBcredentials.json');
+
+const times = getTimes();
+const exportTime = times.fullTime.twoDigit.YY_MM_DD_HH_mm;
+
+DBcredentials.forEach(credential => {
+    const DBname = credential.database;
+
+    mysqldump({
+        connection: credential,
+        dumpToFile: `./back-ups/${DBname}-${exportTime}.sql`,
+    });
+});
